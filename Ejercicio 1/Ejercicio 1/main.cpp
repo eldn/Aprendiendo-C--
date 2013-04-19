1b1862309042b5bd24c81b1ff026f6d336e21534
Jorge Pacora <jpacora@outlook.com>
2013-04-19 11:00:38 -0500

    Primer commit :)

---
+++ Ejercicio 1/Ejercicio 1/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Ejercicio 1
+//
+//  Created by Jorge Pacora on 19/04/13.
+//  Copyright (c) 2013 Jorge Pacora. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    
+    // insert code here...
+    int c = 1;
+    int n;
+    int serie;
+    std::cout << "Cantidad de terminos:\n";
+    std::cin >> n;
+    serie = n;
+    while (c<=n) {
+        std::cout << serie;
+        serie = serie + 1;
+        c = c + 1;
+    }
+    
+    return 0;
+}
+
